52abea7c16345dc6a598e589bd6549961aa52455
kate myer <kate.d.mangubat@gmail.com>
2020-04-25 19:32:00 -0700

    added fx to get card id on click

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import emoji from "emoji-dictionary";
 
 import "./Card.css";
+import axios from "axios";
 //import Axios from "axios";
 
 const Card = (props) => {
+
+  //*need to get id from card
+  const onClickGetCardId = () => {
+    //already have this id={cardh.card.id},  accessible at board.js, so it's a prop
+    const id = props.id
+    props.onDeleteClick(id)
+  }
+
   return (
     <div className="card">
       <section className="card_content">
@@ -13,6 +22,7 @@ const Card = (props) => {
         <section className="card__content-emoji">
           {emoji.getUnicode(`${props.emoji}`)}
         </section>
+        <button onClick={onClickGetCardId}>Delete</button>
       </section>
     </div>
   );
