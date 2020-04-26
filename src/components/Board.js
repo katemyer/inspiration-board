04441cc6991a045117ab4fa30bcb52258caa14d6
kate myer <kate.d.mangubat@gmail.com>
2020-04-25 19:32:23 -0700

    fx for deleting card, rerendering page

--- src/components/Board.js
+++ src/components/Board.js
@@ -8,18 +8,18 @@ import Card from "./Card";
 // import CARD_DATA from "../data/card-data.json";
 
 const Board = (props) => {
-  const API_URl_BASE = `${props.url}/${props.boardName}`;
 
   const [cards, setCards] = useState([]);
   const [errorMessage, setErrorMessage] = useState(null);
 
-  //populate cards
+  //populate cards on start up
   useEffect(() => {
-    axios.get(API_URl_BASE + "/cards") 
+    axios
+      .get(`${props.url}boards/${props.boardName}/cards`) 
       .then((res) => {
         const apiCardsData = res.data;
-        console.log(res)
-        console.log(apiCardsData);
+        //console.log(res)
+        //console.log(apiCardsData);
         setCards(apiCardsData);
       })
       .catch((error) => {
@@ -30,15 +30,61 @@ const Board = (props) => {
 
   const cardComponents = () => {
     return cards.map((cardh) => {
-      console.log(cardh)
+      //console.log(cardh)
       return (
         <section key={cardh.card.id}>
-          <Card id={cardh.card.id} text={cardh.card.text} emoji={cardh.card.emoji} />
+          <Card id={cardh.card.id} onDeleteClick={onDeleteClick} text={cardh.card.text} emoji={cardh.card.emoji} />
         </section>
       );
     });
   };
 
+  //name of function onDeleteClick = delete a card, need to pass in id
+  //Step 1: get the id from the card component in card.js *go see card.js
+  //Step 2: to get to id, need to make a function in the card.js = getCardId()
+  //Step 2a: passing in id, now have access to id from this function getCardId() in card.js
+  //Step 3: passing in onDeleteClick function on CARD object (line 37) so we have access to onDeleteClick function
+  //Step 4: go back to card.js and call this function as props.onDeleteClick(id) so that card has access to it
+  //Step 5: hook up the onClickGetCardId to the <button> in card.js
+  //step 6: complete onDeleteClick function
+  const onDeleteClick = (id) => {
+    // post call to delete card via id
+    console.log("Deleting")
+    axios
+    //example url for delete cards: https://inspiration-board.herokuapp.com/cards/6336
+    //Note A. delete card from API FIRST
+    .delete(`${props.url}cards/${id}`)
+    .then((res) => {
+      const apiDeleteData = res.data;
+      console.log("Deleted")
+      console.log(res)
+      console.log(apiDeleteData);
+      // Step 7. Get all cards via api so it reloads the page after deleting a card
+      //Note B. If delete is successul, GET the cards from API
+      console.log("Getting")
+      axios.get(`${props.url}boards/${props.boardName}/cards`)
+        .then((res) => {
+          console.log("Getted")
+          const apiCardsData = res.data;
+          //console.log(res)
+          //console.log(apiCardsData);
+          console.log("Reloading")
+          //Note C. if GET cards is successful, update front-end here
+          setCards(apiCardsData);
+          console.log("Reloaded")
+        })
+        .catch((error) => {
+          setErrorMessage(error.message);
+          console.log(error.message);
+        });
+    })
+    .catch((error) => {
+      setErrorMessage(error.message);
+      console.log(error.message);
+    });
+
+  }
+  
   return (
     <div className="validation-errors-display">
       {errorMessage && (
