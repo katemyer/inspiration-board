a93d44f29a723b5a8e52ca320129500d1e32e2db
Shonda860 <norshonda.warren@my.ccsu.edu>
2020-04-25 20:30:20 -0700

    updated card CSS

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,27 +3,26 @@ import PropTypes from "prop-types";
 import emoji from "emoji-dictionary";
 
 import "./Card.css";
-import axios from "axios";
-//import Axios from "axios";
 
 const Card = (props) => {
-
   //*need to get id from card
   const onClickGetCardId = () => {
     //already have this id={cardh.card.id},  accessible at board.js, so it's a prop
-    const id = props.id
-    props.onDeleteClick(id)
-  }
+    const id = props.id;
+    props.onDeleteClick(id);
+  };
 
   return (
     <div className="card">
-      <section className="card_content">
-        <p className="card__content-text"> {props.text} </p>
-        <section className="card__content-emoji">
+      <ul className="card__content">
+        <p className="card__content-text">{props.text}</p>
+        <span className="card__content-emoji">
           {emoji.getUnicode(`${props.emoji}`)}
-        </section>
-        <button onClick={onClickGetCardId}>Delete</button>
-      </section>
+        </span>
+        <button className="card__delete" onClick={onClickGetCardId}>
+          Delete
+        </button>
+      </ul>
     </div>
   );
 };
